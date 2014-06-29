0b10bb22b1a42782952cd01df43da573a29509be
Vishal Disawar <disawar2@illinois.edu>
2014-06-28 20:11:25 -0500

    test data for homeless

--- app/routes/homeless.js
+++ app/routes/homeless.js
@@ -2,7 +2,7 @@ var HomelessModel = require('../models/homeless');
 module.exports = function(app){
 	
 	// get all the homeless people
-	app.get('/api/homeless', function(req, res){
+	app.get('/api/homeless/all', function(req, res){
 		HomelessModel.find(function(err, homeless){
 			if(err)
 				res.send(err);
@@ -11,7 +11,7 @@ module.exports = function(app){
 	});
 
 	//get one homeless person
-	app.post('/api/homeless/', function(req, res){
+	app.post('/api/homeless/find', function(req, res){
 		//console.log(req.body);
 		HomelessModel.findById(req.body.homeless_id, function(err, homeless_person){
 			if(err)
@@ -20,5 +20,108 @@ module.exports = function(app){
 		});
 	});
 
+	//create a new homeless person
+	app.post('/api/homeless/create', function(req, res){
+		var data = req.body;
+		var homeless = new HomelessModel(data);
+		homeless.save(function(err){
+			if(err)
+				res.send(err);
+		});
+	});
 
+	// update a homeless person
+	app.post('/api/homeless/update', function(req, res){
+		var data = req.body;
+		HomelessModel.findByIdAndUpdate(req.body.homeless_id, data, function(err){
+			if(err)
+				res.send(err);
+		});
+	});
+
+	// data for testing purposes
+	app.get('/api/test_homeless', function(req, res){
+
+		var supporterArray = [
+			{
+				id : "first",
+				name : "ninja", 
+				amount : 1
+			},
+			{
+				id : "second",
+				name : "monster", 
+				amount : 2
+			}	
+		];
+
+		var needs = {
+			food : 
+			{ 
+				current : 1,
+				total : 2
+			},
+			shelter : 
+			{
+				current : 3, 
+				total : 4
+			},
+			clothing : 
+			{
+				current : 5,
+				total : 6
+			}
+		};
+
+		var goalsArray = [
+			{
+				title : "get fat",
+				subgoals:
+					[
+						{
+							subgoaltitle : "eat",
+							current : 200,
+							total : 300
+						},
+						{
+							subgoaltitle : "cookies",
+							current : 100,
+							total : 500
+						}
+					]
+			},
+			{
+				title : "get skinny",
+				subgoals:
+					[
+						{
+							subgoaltitle : "eat less",
+							current : 2,
+							total : 3
+						},
+						{
+							subgoaltitle : "monkey",
+							current : 4,
+							total : 8
+						}
+					]
+			}
+		];
+
+
+		var homeless = new HomelessModel({
+			name : "Vishal",
+			pictureUrl : "http://yo.com",
+			bio : "Ma Life",
+			supporters : supporterArray,
+			dailyNeeds : needs,
+			goals : goalsArray
+		});
+		
+		homeless.save(function(err){
+			if(err)
+				res.send(err);
+			res.send('\n' + homeless);
+		});
+	});
 };
